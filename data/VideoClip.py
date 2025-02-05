77c00933afb412277c79a71afcf17af8290ab28c
JeppeGivskud <jgivsk20@student.aau.dk>
2025-02-05 09:26:34 +0100

    Made it a little more general

--- data/VideoClip.py
+++ data/VideoClip.py
@@ -1,8 +1,8 @@
 from video_main import EmotionAnalysisVideo
 import os
 print("Starting")
-print("Video file exists:", os.path.exists('/realtime-facial-emotion-analyzer/data/jeppe.mp4'))
-print("Video file exists:", os.path.exists('data/jeppe.mp4'))
+print("Video file exists:", os.path.exists('/realtime-facial-emotion-analyzer/data/input.mp4'))
+print("Video file exists:", os.path.exists('data/input.mp4'))
 
 print("Current working directory:", os.getcwd())
 # You can pick a face detector depending on Acc/speed requirements
@@ -19,20 +19,20 @@ print("Analyser made")
 print("Starting Emotion Analysis on Video")
 # Using relative paths for file access
 print("Starting")
-print("Video file exists:", os.path.exists('data/jeppe.mp4'))
+print("Video file exists:", os.path.exists('data/input.mp4'))
 print("Output file exists:", os.path.exists('data/output.mp4'))
 
 print("Current working directory:", os.getcwd())
 
 emotion_recognizer.emotion_analysis_video(
-    video_path='/realtime-facial-emotion-analyzer/data/jeppe.mp4',
+    video_path='/realtime-facial-emotion-analyzer/data/input.mp4',
     detection_interval=1,
     save_output=True,
     preview=False,  # Ensure this is set to False
     output_path="/realtime-facial-emotion-analyzer/data/output.mp4",
     resize_scale=0.5,
 )
-print("Video file exists:", os.path.exists('data/jeppe.mp4'))
+print("Video file exists:", os.path.exists('data/input.mp4'))
 print("Output file exists:", os.path.exists('data/output.mp4'))
 
 
